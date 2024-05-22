904096e0e7fa9ecf84196f6cca8b92723aa87538
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-05-22 16:53:33 -0300

    refactor: Remove unnecessary code in create-account.go

--- bank/internal/routes/bank/create-account.go
+++ bank/internal/routes/bank/create-account.go
@@ -1,11 +1,9 @@
 package bank
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/jnaraujo/tec502-inter-bank/bank/internal/interbank"
 	"github.com/jnaraujo/tec502-inter-bank/bank/internal/storage"
 	"github.com/jnaraujo/tec502-inter-bank/bank/internal/validate"
 )
@@ -19,9 +17,6 @@ func CreateAccountRoute(c *fiber.Ctx) error {
 	if errs := validate.ParseAndValidate(c.Body(), &body); len(errs) > 0 {
 		return c.Status(http.StatusBadRequest).JSON(&fiber.Map{"error": errs})
 	}
-	userKey := interbank.NewUserKey(255, 1312312300)
-	fmt.Println(userKey.String())
-	fmt.Println(interbank.NewUserKeyFromStr("12-1312312300"))
 
 	user := storage.CreateUser(body.Name)
 	return c.Status(http.StatusCreated).JSON(&user)
