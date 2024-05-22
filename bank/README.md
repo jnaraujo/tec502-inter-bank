ef2bd6dfc580a24161d89f0fdac14ea3a14315a3
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-05-21 22:15:10 -0300

    feat: add user key

--- bank/README.md
+++ bank/README.md
@@ -17,3 +17,19 @@
 ## Rotas do InterBank
 ### Transferências
 - POST /interbank/transfer - Realiza uma transferência entre contas de bancos diferentes
+
+
+# Protocolo de comunicação InterBank
+O InterBank é um protocolo de comunicação que permite a transferência de valores entre contas de bancos diferentes. Ele é baseado em requisições HTTP e JSON.
+
+## Chave do usuário
+Cada usuário possui uma chave única de 4 bytes (32 bits) que é utilizada para identificá-lo. Essa chave é gerada pelo banco e é utilizada para identificar o usuário em todas as transações. A chave é formada pela concatenação de um identificador do banco (1 byte) e um identificador do usuário (3 bytes).
+
+### Identificador do banco
+O identificador do banco é um número inteiro não negativo que identifica o banco. Ele é utilizado para garantir que a chave do usuário seja única entre todos os bancos. O identificador do banco é um número de 1 byte (8 bits).
+
+### Identificador do usuário
+O identificador do usuário é um número inteiro não negativo que identifica o usuário dentro do banco. Ele é utilizado para garantir que a chave do usuário seja única dentro do banco. O identificador do usuário é um número de 3 bytes (24 bits).
+
+### Exemplo
+Suponha que o banco tenha o identificador 1 e o usuário tenha o identificador 1234. A chave do usuário seria a concatenação de 1 e 1234, ou seja, 1-1234.
---
+++ bank/internal/interbank/user_key.go
@@ -0,0 +1,70 @@
+package interbank
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"math"
+	"strconv"
+)
+
+type BankId [2]byte
+type UserId [4]byte
+
+type UserKey struct {
+	bank BankId
+	user UserId
+}
+
+func NewUserKeyFromStr(value string) (*UserKey, error) {
+	if !bytes.Contains([]byte(value), []byte("-")) {
+		return nil, errors.New("invalid user key format")
+	}
+	bankIdStr := bytes.Split([]byte(value), []byte("-"))[0]
+	userIdStr := bytes.Split([]byte(value), []byte("-"))[1]
+
+	bankId, err := strconv.Atoi(string(bankIdStr))
+	if err != nil {
+		return nil, err
+	}
+	if bankId > math.MaxUint16 {
+		return nil, errors.New("invalid bank id")
+	}
+
+	userId, err := strconv.Atoi(string(userIdStr))
+	if err != nil {
+		return nil, err
+	}
+	if userId > math.MaxUint32 {
+		return nil, errors.New("invalid user id")
+	}
+
+	return NewUserKey(uint16(bankId), uint32(userId)), nil
+}
+
+func NewUserKey(bankId uint16, userId uint32) *UserKey {
+	bank := BankId{}
+	user := UserId{}
+
+	binary.BigEndian.PutUint16(bank[:], bankId)
+	binary.BigEndian.PutUint32(user[:], userId)
+
+	return &UserKey{
+		bank: bank,
+		user: user,
+	}
+}
+
+func (uk *UserKey) Bytes() []byte {
+	b := make([]byte, 6)
+	copy(b[:2], uk.bank[:])
+	copy(b[2:], uk.user[:])
+	return b
+}
+
+func (uk *UserKey) String() string {
+	bankId := binary.BigEndian.Uint16(uk.bank[:])
+	userId := binary.BigEndian.Uint32(uk.user[:])
+	return fmt.Sprintf("%d-%d", bankId, userId)
+}
