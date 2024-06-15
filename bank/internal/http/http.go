ed9592fa038b0c5630a36b283171efdc52564cac
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-06-15 19:26:31 -0300

    refactor: Update bank routes registration to use separate package for routes

--- bank/internal/http/http.go
+++ bank/internal/http/http.go
@@ -5,8 +5,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/jnaraujo/tec502-inter-bank/bank/internal/routes/bank"
-	"github.com/jnaraujo/tec502-inter-bank/bank/internal/routes/interbank"
+	"github.com/jnaraujo/tec502-inter-bank/bank/internal/routes"
 )
 
 func NewServer(port int) error {
@@ -17,33 +16,12 @@ func NewServer(port int) error {
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
-	registerBankRoutes(app)
-	registerInterBankRoutes(app)
+	routes.RegisterBankRoutes(app)
+	routes.RegisterInterBankRoutes(app)
 
 	err := app.Listen(fmt.Sprintf("%s:%d", "0.0.0.0", port))
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-func registerBankRoutes(app *fiber.App) {
-	router := app.Group("/api")
-
-	router.Get("/", bank.GetRootRoute)
-
-	router.Post("/accounts", bank.CreateAccountRoute)
-	router.Get("/accounts/:id", bank.FindAccountRoute)
-	router.Delete("/accounts/:id", bank.DeleteAccountRoute)
-	router.Get("/accounts/:id/transactions", bank.ListAccountTransactionsRoute)
-
-	router.Post("/payments/deposit", bank.DepositRoute)
-	router.Post("/payments/pay", bank.PayRoute)
-}
-
-func registerInterBankRoutes(app *fiber.App) {
-	router := app.Group("/interbank")
-
-	router.Post("/add-credit", interbank.AddCreditRoute)
-	router.Post("/sub-credit", interbank.SubCreditRoute)
 }
---
+++ bank/internal/routes/routes.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jnaraujo/tec502-inter-bank/bank/internal/routes/bank"
+	"github.com/jnaraujo/tec502-inter-bank/bank/internal/routes/interbank"
+)
+
+func RegisterInterBankRoutes(app *fiber.App) {
+	router := app.Group("/interbank")
+
+	router.Post("/add-credit", interbank.AddCreditRoute)
+	router.Post("/sub-credit", interbank.SubCreditRoute)
+}
+
+func RegisterBankRoutes(app *fiber.App) {
+	router := app.Group("/api")
+
+	router.Get("/", bank.GetRootRoute)
+
+	router.Post("/accounts", bank.CreateAccountRoute)
+	router.Get("/accounts/:id", bank.FindAccountRoute)
+	router.Delete("/accounts/:id", bank.DeleteAccountRoute)
+	router.Get("/accounts/:id/transactions", bank.ListAccountTransactionsRoute)
+
+	router.Post("/payments/deposit", bank.DepositRoute)
+	router.Post("/payments/pay", bank.PayRoute)
+}
