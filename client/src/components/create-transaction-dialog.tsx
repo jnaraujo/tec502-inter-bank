8f6989fb118e03e7b807728d9bfceaaac1801b96
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-06-25 19:25:21 -0300

    refactor: Update TransferBox component to handle multiple operations and add transaction dialog

---
+++ client/src/components/create-transaction-dialog.tsx
@@ -0,0 +1,68 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+
+export interface Operation {
+  to: string
+  amount: number
+}
+
+interface Props {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+  onOperationCreated: (operation: Operation) => void
+}
+
+export function CreateTransactionDialog(props: Props) {
+  function handleSendCommand(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    props.onOperationCreated({
+      to: formData.get("to") as string,
+      amount: Number(formData.get("amount")),
+    })
+
+    props.onOpenChange(false)
+  }
+
+  return (
+    <Dialog open={props.open} onOpenChange={props.onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar nova operação</DialogTitle>
+          <DialogDescription>
+            Todas as operação atômicas. Ou seja, se uma falhar, todas serão
+            desfeitas (até as que tiveram sucesso).
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          <form className="space-y-4" onSubmit={handleSendCommand}>
+            <div className="space-y-1">
+              <Label htmlFor="to">IBK do beneficiário:</Label>
+              <Input id="to" name="to" placeholder="Ex: 1-203" required />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="amount">Valor:</Label>
+              <Input
+                id="amount"
+                name="amount"
+                placeholder="Ex: 500"
+                type="number"
+                required
+              />
+            </div>
+            <Button type="submit">Adicionar operação</Button>
+          </form>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- client/src/components/transfer-box.tsx
+++ client/src/components/transfer-box.tsx
@@ -1,25 +1,93 @@
+import { ArrowLeftRight, DollarSign, Plus, Trash } from "lucide-react"
+import { useState } from "react"
+import { CreateTransactionDialog, Operation } from "./create-transaction-dialog"
 import { Button } from "./ui/button"
-import { Input } from "./ui/input"
-import { Label } from "./ui/label"
 
 export function TransferBox() {
-  return (
-    <article className="space-y-4 rounded-lg border bg-background p-6">
-      <h2 className="text-lg font-medium text-zinc-500">
-        Realizar transferência
-      </h2>
+  const [operations, setOperations] = useState<Array<Operation>>([])
+  const [openDialog, setOpenDialog] = useState(false)
 
-      <form className="space-y-4">
-        <div className="space-y-1">
-          <Label htmlFor="to">IBK do beneficiário:</Label>
-          <Input placeholder="Ex: 1-203" required />
-        </div>
-        <div className="space-y-1">
-          <Label htmlFor="to">Valor:</Label>
-          <Input placeholder="Ex: 500" type="number" required />
-        </div>
-        <Button type="submit">Realizar transferência</Button>
-      </form>
-    </article>
+  function handleNewOperation(operation: Operation) {
+    setOperations((prev) => [operation, ...prev])
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+  }
+
+  function removeOperation(idx: number) {
+    setOperations((prev) => prev.filter((_, i) => i !== idx))
+  }
+
+  return (
+    <>
+      <article className="space-y-4 rounded-lg border bg-background p-6">
+        <h2 className="text-lg font-medium text-zinc-800">
+          Realizar transferência
+        </h2>
+        <p className="text-zinc-500">
+          Sua transferência pode ter várias operações! Se uma delas falhar,
+          todas são desfeitas.
+        </p>
+
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <h3 className="font-medium text-zinc-600">
+            Operações da transferência
+          </h3>
+          <div>
+            <div className="h-64 space-y-2 overflow-y-auto overflow-x-hidden px-4">
+              {operations.length === 0 ? (
+                <p className="p-6 text-center text-sm text-zinc-500">
+                  Sem operações ainda. Adicione uma nova operação para aparecer
+                  aqui.
+                </p>
+              ) : null}
+              {operations.map((op, idx) => (
+                <div className="flex items-center gap-2 text-zinc-400">
+                  <div className="flex w-full items-center gap-2">
+                    <div className="flex items-center gap-1">
+                      <ArrowLeftRight className="size-4 text-zinc-600" />
+                      <span className="min-w-20 text-zinc-500">
+                        Para: {op.to}
+                      </span>
+                    </div>
+                    <div className="flex items-center gap-1">
+                      <DollarSign className="size-4 text-green-600" />
+                      <span className="text-green-500">
+                        R$ {op.amount.toFixed(2)}
+                      </span>
+                    </div>
+                  </div>
+                  <button
+                    onClick={() => {
+                      removeOperation(idx)
+                    }}
+                  >
+                    <Trash className="size-5 text-red-400 transition-colors duration-200 hover:text-red-500" />
+                  </button>
+                </div>
+              ))}
+            </div>
+            <Button
+              className="mt-4 flex items-center gap-1"
+              variant="secondary"
+              onClick={() => {
+                setOpenDialog(true)
+              }}
+            >
+              <Plus /> Adicionar nova Operação
+            </Button>
+          </div>
+
+          <Button type="submit">Realizar transferência</Button>
+        </form>
+      </article>
+
+      <CreateTransactionDialog
+        open={openDialog}
+        onOpenChange={setOpenDialog}
+        onOperationCreated={handleNewOperation}
+      />
+    </>
   )
 }
