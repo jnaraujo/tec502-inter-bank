2f3c66a4f9dbfc1b17650e4ab3abc0bb6409be5a
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-06-25 19:26:54 -0300

    refactor: Improve WelcomeBox component and update welcome message styling

--- client/src/components/welcome-box.tsx
+++ client/src/components/welcome-box.tsx
@@ -8,7 +8,7 @@ export function WelcomeBox() {
       <h1 className="text-xl font-medium text-zinc-900">
         Bem-vindo, {user?.name}! (IBK: {user?.ibk})
       </h1>
-      <p>
+      <p className="text-zinc-500">
         Você está usando o nosso Sistema Inter Bancário (InterBank). Com ele,
         você pode realizar transações entre suas contas em diferentes bancos no
         consorcio.
