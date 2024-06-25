41be3a42b9dde8e5a7eeadb6dbb302816d85bb71
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-06-25 19:15:37 -0300

    refactor: Update TransactionListBox component to use a darker shade of zinc for the title

--- client/src/components/transaction-list-box.tsx
+++ client/src/components/transaction-list-box.tsx
@@ -3,7 +3,7 @@ import { TransactionBox } from "./transaction-box"
 export function TransactionListBox() {
   return (
     <article className="flex flex-col justify-between space-y-4 overflow-auto rounded-lg border bg-background p-6">
-      <h2 className="text-xl font-medium text-zinc-900">Suas transações</h2>
+      <h2 className="text-xl font-medium text-zinc-800">Suas transações</h2>
 
       <div className="flex flex-col gap-6 overflow-auto py-2">
         <TransactionBox
