9112608a153dd3bfce3487d30ac89056f0a3c6d3
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-06-25 19:51:14 -0300

    feat: Add @tanstack/react-query and @tanstack/eslint-plugin-query dependencies

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { createRouter, RouterProvider } from "@tanstack/react-router"
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen"
@@ -15,8 +16,12 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
