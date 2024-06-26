78898fe91e9b9a4d7bc4a687c819ff25f69690a1
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-06-26 18:45:29 -0300

    chore: dockerfile stuff

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,7 +1,19 @@
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from "@/contexts/auth-context"
 import { createRootRoute, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+import React from "react"
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      )
 
 export const Route = createRootRoute({
   component: () => (
