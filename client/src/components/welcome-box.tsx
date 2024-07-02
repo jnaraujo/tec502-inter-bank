577d903bda5bc0ef1c335d32551c3302c575db06
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-07-02 19:42:36 -0300

    chore: Update welcome box layout and content

--- client/src/components/welcome-box.tsx
+++ client/src/components/welcome-box.tsx
@@ -17,16 +17,29 @@ export function WelcomeBox() {
   }
 
   return (
-    <article className="flex h-fit flex-col justify-between space-y-2 rounded-lg border bg-background p-6">
-      <h1 className="text-xl font-medium text-zinc-900">
-        Bem-vindo, {user?.name}! (IBK: {user?.ibk} -{" "}
-        {userTypeToHuman(user?.type)})
-      </h1>
-      <p className="text-zinc-500">
-        Você está usando o nosso Sistema Inter Bancário (InterBank). Com ele,
-        você pode realizar transações entre suas contas em diferentes bancos no
-        consorcio.
-      </p>
+    <article className="grid h-fit grid-cols-2 gap-6 rounded-lg border bg-background p-6">
+      <div className="space-y-2">
+        <h1 className="text-xl font-medium text-zinc-900">
+          Bem-vindo, {user?.name}!
+        </h1>
+        <p className="text-zinc-500">
+          Você está usando o nosso Sistema Inter Bancário (InterBank). Com ele,
+          você pode realizar transações entre suas contas em diferentes bancos
+          no consorcio.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-lg font-medium text-zinc-900">Dados da Conta:</h2>
+        <ul className="space-y-1">
+          <li className="text-sm text-zinc-500">IBK: {user?.ibk}</li>
+          <li className="text-sm text-zinc-500">
+            Tipo: {userTypeToHuman(user?.type)}
+          </li>
+          <li className="text-sm text-zinc-500">
+            Documento(s): {user?.documents.join(", ")}
+          </li>
+        </ul>
+      </div>
     </article>
   )
 }
