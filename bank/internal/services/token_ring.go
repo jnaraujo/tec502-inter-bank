34f08e6b70449eb31dff9f5e5a53168622c9f805
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-07-01 23:26:27 -0300

    refactor: Add error logging in token_ring.go

--- bank/internal/services/token_ring.go
+++ bank/internal/services/token_ring.go
@@ -113,6 +113,8 @@ func findNextValidBank(id interbank.BankId) *interbank.BankId {
 
 	res, err := http.Get("http://" + bank.Addr + "/interbank/token/ok")
 	if err != nil || res.StatusCode != http.StatusOK {
+		slog.Error("Banco não respondeu ao token", "bank", bank.Id, "error", err)
+
 		nextBank := storage.Ring.Next(id)
 		if nextBank == nil {
 			panic("Não tem banco no Token Ring!")
