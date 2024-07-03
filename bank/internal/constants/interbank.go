83bfb54a283c09d0c0633f1fdff0a86e77520dfa
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-07-03 18:14:00 -0300

    refactor: Improve interbank constants

--- bank/internal/constants/interbank.go
+++ bank/internal/constants/interbank.go
@@ -2,12 +2,14 @@ package constants
 
 import "time"
 
+// Constantes para o tempo de espera de operações interbancárias
 const (
 	OperationTimeout                  = 250 * time.Millisecond // Tempo máximo que o banco espera por uma resposta de outro banco para uma operação
 	MaxTimeToProcessLocalTransactions = 10 * time.Second       // Tempo máximo que o banco tem para processar transações locais
 	MaxWaitTimeForTokenInterBank      = 15 * time.Second       // Tempo máximo que o banco espera por um token de outro banco quando ele deveria ser o próximo
 )
 
+// Constantes para o tempo de espera de tokens
 const (
 	MaxTimeToRequestToken = 250 * time.Millisecond
 )
